b4d574ce664f5d47629d8b750a661ff37f1aac42
JAVEX JAVA <87818715+Jerin3j@users.noreply.github.com>
2021-09-19 23:18:00 +0530

    Create antitheri.js

---
+++ plugins/antitheri.js
@@ -0,0 +1,36 @@
+/* Copyright (C) 2020 afnanplk.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+WhatsAsena - Yusuf Usta
+*/
+
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const exec = require('child_process').exec;
+const os = require("os");
+const fs = require('fs');
+const Config = require('../config')
+const Language = require('../language');
+const Lang = Language.getString('evaluators');
+const SLang = Language.getString('conventer');
+const NLang = Language.getString('scrapers');
+const googleTTS = require('google-translate-tts');
+const Heroku = require('heroku-client');
+const heroku = new Heroku({
+    token: Config.HEROKU.API_KEY
+});
+let baseURI = '/apps/' + Config.HEROKU.APP_NAME;
+
+
+async function checkUsAdmin(message, user = message.data.participant) {
+    var grup = await message.client.groupMetadata(message.jid);
+    var sonuc = grup['participants'].map((member) => {     
+        if (member.jid.split("@")[0] == user.split("@")[0] && member.isAdmin) return true; else; return false;
+    });
+    return sonuc.includes(true);
+}
+async function checkImAdmin(message, user = message.client.user.jid) {
+    var grup = await message.client.groupMetadata(message.jid);
+    var sonuc = grup['participants'].map((member) => {     
+        if (member.jid.split("@")[0] == user.split("@")[0] && member.isAdmin) return true; else; return false;
+    });
