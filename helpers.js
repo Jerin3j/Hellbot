6a7dbf6986e80fc72ec5e028bdb7a3b91ff21618
JAVEX JAVA <87818715+Jerin3j@users.noreply.github.com>
2021-09-20 08:57:37 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *WhatsAsena*:  ```" + msg + "```"
+    return "✅ *Hellbot*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *WhatsAsena*:  ```" + msg + "```"
+    return "🛑 *Hellbot*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *WhatsAsena*:  ```" + msg + "```"
+    return "⏺️ *Hellbot*:  ```" + msg + "```"
 }
 
 
