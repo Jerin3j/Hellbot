b1d9f06629fa39e47e689577542296f51eece21e
JAVEX JAVA <87818715+Jerin3j@users.noreply.github.com>
2021-09-30 06:55:37 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -262,7 +262,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp...')}`);
                                     , MessageType.text);
                             } else {
                                 await conn.sendMessage(conn.user.jid, '*~_________~ Hellbot ~______~*' +
-                                    '\n*🌀error' +
+                                    '\n*🌀 error' +
                                     '\n\n*⚠️ ' + error + '*\n'
                                     , MessageType.text);
                             }
